6ffa8fbe160d045a0c2747ec246e5a226d32142a
Akshat Sharma <akshatmsh69@gmail.com>
2026-08-14 09:57:28 +0530

    task input js updated

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// variable declare
+// variables
 let isSidebarOpen = false;
-const todo = document.getElementById("input")
+let task = "";
+let date = "";
+let time = "";
+let category = "";
 
+// getting html elements
 
-let todoText = ""
-
-todo.addEventListener("change",(event) => {
-  console.log(event.target.value);
-  todoText = event.target.value
-  
-  
-})
-
-// getting elements from html
 const sidePanel = document.getElementById("sidepanel");
 const hamburger = document.getElementById("hamburger");
 const overlay = document.getElementById("overlay");
+const taskInput = document.getElementById("input");
+const timeInput = document.getElementById("time");
+const dateInput = document.getElementById("date");
+const categoryInput = document.getElementById("Category");
+const taskList = document.getElementById("taskListVeiw");
 
+// functions
 
-
-// all functions
 const toggleSideBar = () => {
   isSidebarOpen = !isSidebarOpen;
 
@@ -34,8 +32,44 @@ const toggleSideBar = () => {
   }
 };
 
+addTodo = (addtask) => {
+  taskList.innerHTML = `<li class="lst" id="lst">
+  <div>
+  <span style="font-size: 20px;" id="task"><b>Task</b></span>
+  <span style="font-size: 20px;">:</span>
+  <span style="font-size: 20px;"><b>${task}</b></span>
+  </div>
+  <div>
+  <span>Date</span>
+  <span>:</span>
+  <span>${date}</span>
+  </div>
+  <div>
+  <span>Time</span>
+          <span>:</span>
+          <span>${time}</span>
+        </div>
+        <div>
+          <span>Category</span>
+          <span>:</span>
+          <span>${category}</span>
+        </div>
+      </li>`;
+};
+// eventlistners
 
-
-// event listners
 hamburger.addEventListener("click", toggleSideBar);
 
+taskInput.addEventListener("change", (event) => {
+  console.log(event.target.value);
+  task = event.target.value;
+});
+timeInput.addEventListener("change", (event) => {
+  time = event.target.value;
+});
+dateInput.addEventListener("change", (event) => {
+  date = event.target.value;
+});
+categoryInput.addEventListener("change", (event) => {
+  category = event.target.value;
+});
